a424045d033068e640369a1cb62bb175b7e0988d
Shushan Hovhannisayn <shushan_hov@yahoo.com>
2019-06-06 17:16:58 +0400

    a

--- HomeWork8/8.js
+++ HomeWork8/8.js
@@ -5,16 +5,36 @@ addButtonActs.addEventListener('click',whenAddButtonClicked);
 newToDoElementName.addEventListener('keypress',whenKeyPressed);
 
 function    whenAddButtonClicked (){
-    
     const newToDoElement=document.createElement('div');
-    const dne
-    newToDoElement.innerText=newToDoElementName.value;
-    //newToDoElement.classList.add('todo-item');
+    const checkDone=document.createElement('button');
+    checkDone.innerText='X';
+    checkDone.setAttribute('type','button');
+    newToDoElement.addEventListener('click',whenStatusChanged);
+    checkDone.addEventListener('click',whenchekDoneClicked);
+    newToDoElement.innerHTML=newToDoElementName.value;
+    newToDoElement.appendChild(checkDone);
+    newToDoElement.classList.add('todo-item');
     const allToDoes=document.getElementById("todo-items-list");
     allToDoes.appendChild(newToDoElement);
+    
+
+   
+
     newToDoElementName.value='';
+    
 }
 
 function whenKeyPressed(e){
     if (e.which===13) whenAddButtonClicked();
 }
+
+function whenchekDoneClicked(e){
+    const currenElement=e.target;
+    currenElement.parentElement.remove();
+    e.stopPropagation();
+}
+function whenStatusChanged(e){
+    const currenTask=e.currentTarget;
+    currenTask.classList.toggle('completed');
+}
+
